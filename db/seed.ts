6c562109575bd878d288891c77f3c3806fc6d1e6
MakoVoltron <30395776+MakoVoltron@users.noreply.github.com>
2025-07-04 20:32:55 +0200

    Changes to import of Prisma in seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from "@/lib/generated/prisma";
+// import { PrismaClient } from "@/lib/generated/prisma";
+
+import { PrismaClient } from "@prisma/client";
 import sampleData from "./sample-data";
 
 async function main() {
