66b587066bd5729a22e391d6507ae7275cbc7fc0
MakoVoltron <30395776+MakoVoltron@users.noreply.github.com>
2025-08-03 15:42:45 +0200

    Resend email receipts

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -6,12 +6,13 @@ import { getMyCart } from "./cart.actions";
 import { getUserById } from "./user.actions";
 import { insertOrderSchema } from "../validators";
 import { prisma } from "@/db/prisma";
-import { CartItem, PaymentResult } from "@/types";
+import { CartItem, PaymentResult, ShippingAddress } from "@/types";
 import { auth } from "@/auth";
 import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
 import { PAGE_SIZE } from "../constants";
 import { Prisma } from "@prisma/client";
+import { sendPurchaseReceipt } from "@/email";
 
 // Create order and create the order items
 export async function createOrder() {
@@ -248,6 +249,16 @@ export async function updateOrderToPaid({
   });
 
   if (!updateOrder) throw new Error("Order not found");
+
+  console.log("from updateOrderToPaid action: ", updateOrder);
+
+  sendPurchaseReceipt({
+    order: {
+      ...updateOrder,
+      shippingAddress: updateOrder.shippingAddress as ShippingAddress,
+      paymentResult: updateOrder.paymentResult as PaymentResult,
+    },
+  });
 }
 
 // Get user's orders
